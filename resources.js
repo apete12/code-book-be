d7d352243338ad37d563c6e55c3076c29b5db15b
Alex Peterson <aspeterson.126@gmail.com>
2023-11-05 18:30:53 -0600

    Refactor pets to code

---
+++ resources.js
@@ -0,0 +1,81 @@
+let resources = [
+  {
+    id: 1, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Article'
+  },
+  {
+    id: 2, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Article'
+  },
+  {
+    id: 3,
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Video'
+  },
+  {
+    id: 4, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Video'
+  },
+  {
+    id: 5, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Article'
+  },
+  {
+    id: 6, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Article'
+  },
+  {
+    id: 7, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Video'
+  },
+  {
+    id: 8, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Video'
+  },
+  {
+    id: 9, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Article'
+  },
+  {
+    id: 10, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Video'
+  },
+  {
+    id: 11, 
+    name: '', 
+    details: '', 
+    link: '',
+    type: 'Article'
+  }
+]
+
+export default resources
---
+++ server.js
@@ -0,0 +1,72 @@
+import resources from './resources';
+
+const express = require('express');
+const app = express();
+app.locals.resources = [];
+app.use(express.json());
+const cors = require('cors');
+app.use(cors());
+
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Code Book';
+
+app.locals.resources = resources
+
+app.get('/api/v1/resources', (request, response) => {
+  const resources = app.locals.resources;
+
+  response.json({ resources });
+});
+
+app.get('/api/v1/resources/:id', (request, response) => {
+  const { id } = request.params;
+
+  const pet = app.locals.resources.find(resource => resource.id === parseInt(id));
+  if (!resource) {
+    return response.sendStatus(404);
+  }
+
+  response.status(200).json(resource);
+});
+
+app.post('/api/v1/resources', (request, response) => {
+  const id = Date.now();
+  const resource = request.body;
+
+  for (let requiredParameter of ['name', 'details']) {
+    if (!resource[requiredParameter]) {
+      response
+        .status(422)
+        .send({ error: `Expected format: { name: <String>, details: <String> }. You're missing a "${requiredParameter}" property.` });
+      return
+    }
+  }
+
+  const { name, details, type, link } = resource;
+  app.locals.resources.push({ id, name, details, type, link});
+  response.status(201).json({ id, name, details, type, link});
+});
+
+app.delete('/api/v1/resources/:id', (request, response) => {
+  const { id } = request.params;
+
+  const resourceToDelete = app.locals.resources.find((resource) => {
+    return resource.id === parseInt(id)
+  });
+
+  if (!resourceToDelete) {
+    return response.sendStatus(404);
+  }
+
+  const filteredResources = app.locals.resources.filter(resource => resource.id != id);
+
+  app.locals.resources = filteredResources
+
+  response.sendStatus(204);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
+
+module.exports = app
