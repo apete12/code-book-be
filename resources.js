e9942b1495cbbf3f5695a773e75a17eb9b30b90d
Alex Peterson <aspeterson.126@gmail.com>
2023-11-06 12:10:16 -0600

    Add notes to data model

--- resources.js
+++ resources.js
@@ -3,7 +3,7 @@ let resources = [
     id: 1, 
     name: 'WAI Tutorials', 
     details: 'Collection of tutorials shows you how to develop web content that is accessible to people with disabilities', 
-    intention: 'I plan to use this resources when I review my project Code Book, in order to ensure it is fully accessible',
+    notes: 'I plan to use this resources when I review my project Code Book, in order to ensure it is fully accessible',
     link: 'https://www.w3.org/WAI/tutorials/',
     type: 'Article'
   },
@@ -11,7 +11,7 @@ let resources = [
     id: 2, 
     name: 'Accessibility Tree - MDN', 
     details: 'The accessibility tree contains accessibility-related information for most HTML elements.', 
-    intention: 'I plan to use this resources when I review my project Code Book, in order to ensure it is fully accessible',
+    notes: 'I plan to use this resources when I review my project Code Book, in order to ensure it is fully accessible',
     link: 'https://developer.mozilla.org/en-US/docs/Glossary/Accessibility_tree',
     type: 'Article'
   },
@@ -19,7 +19,7 @@ let resources = [
     id: 3,
     name: 'PostgreSQL - Youtube Video', 
     details: 'Setting up postgresql server on frontend', 
-    intention: 'I plan to use this resources when I refactor Code Book to implement a database using PostgreSQL',
+    notes: 'I plan to use this resources when I refactor Code Book to implement a database using PostgreSQL',
     link: 'https://www.youtube.com/watch?v=O4bNwkC1ZxA',
     type: 'Video'
   },
@@ -27,23 +27,24 @@ let resources = [
     id: 4, 
     name: 'GraphQL Youtube', 
     details: 'Frontend resource for graphql queries and mutations', 
+    notes: 'I plan to watch this video to gain a deeper understanding of how to execute GraphQL queries and mutations on the frontend for my projects.',
     link: 'https://www.youtube.com/watch?v=YyUWW04HwKY',
     type: 'Video'
   },
   {
     id: 5, 
     name: 'WCAG Quick Reference', 
-    details: '', 
-    intention: '',
+    details: 'Provides a convenient way to check and implement accessibility standards. This reference guide offers concise summaries of the key WCAG 2.1 guidelines, enabling you to ensure your projects are accessible to a wide audience.', 
+    notes: 'Plan to use this quick reference guide to quickly check and ensure my projects comply with WCAG 2.1 accessibility guidelines, guaranteeing a more inclusive user experience.',
     link: 'https://www.w3.org/WAI/WCAG22/quickref/?versions=2.1',
     type: 'Article'
   },
   {
     id: 6, 
     name: 'TypeScript Tutorial', 
-    details: '', 
+    details: 'Dive into TypeScript with this comprehensive tutorial, available on YouTube. Gain a solid understanding of TypeScript\'s features, benefits, and best practices. This resource is perfect for developers looking to enhance code quality and maintainability through strong typing.', 
     link: 'https://www.youtube.com/watch?v=FJDVKeh7RJI&t=1475s',
-    intention: 'I plan to use this resources when I refactor Code Book to learn and implement TypeScript',
+    notes: 'I plan to use this resources when I refactor Code Book to learn and implement TypeScript',
     type: 'Video'
   }
 ]
